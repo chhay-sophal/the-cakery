5ccc70bfb06103365882c114b4a3a46ff8d25334
jayjayhyun <138762092+jayjayhyun@users.noreply.github.com>
2024-06-28 20:36:14 +0700

    Homepage FrontEnd Design

--- thecakery/base/views.py
+++ thecakery/base/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render
+from django.urls import path
 from cakes.models import Cake, CakeImage
 
 def home_page(request): 
     cakes = Cake.objects.all()
     cake_images = CakeImage.objects.all()
-    return render(request, "base/home.html", {'cakes': cakes, 'cake_images': cake_images})
+
+    return render(request, "base/homepage.html", {'cakes': cakes, 'cake_images': cake_images})
+
+
+
