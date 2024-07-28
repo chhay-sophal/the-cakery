5170a933aee7b02dcb0c859e7aa18eabfc45b281
jayjayhyun <138762092+jayjayhyun@users.noreply.github.com>
2024-07-28 16:39:13 +0700

    New Design
    The Cakery Front End Added Design

--- thecakery/base/urls.py
+++ thecakery/base/urls.py
@@ -1,6 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from .views import *
+from .views import home_page, about, contact,catalog
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('catalog/', catalog, name='catalog'),
 ]
--- thecakery/base/views.py
+++ thecakery/base/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, render
-from django.urls import path
+from django.shortcuts import render
 from cakes.models import Cake, CakeImage
 
-def home_page(request): 
+def home_page(request):
     cakes = Cake.objects.all()
     cake_images = CakeImage.objects.all()
-
     return render(request, "base/homepage.html", {'cakes': cakes, 'cake_images': cake_images})
 
+def about(request):
+    return render(request, 'base/about_us.html')
 
-
+def contact(request):
+    return render(request, 'base/contact_us.html')
+def catalog(request):
+    return render(request, 'base/catalog.html')
