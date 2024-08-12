fa8a0c500409c2454bcbc5b2a1f67ce5dff2d42b
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-08-12 14:49:55 +0700

    login, logout, signup

--- thecakery/accounts/views.py
+++ thecakery/accounts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import CustomUserCreationForm 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html', {'user': request.user})
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def logout_confirm(request):
+    return render(request, 'registration/logout_confirm.html')
