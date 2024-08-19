7cdcb016992e0dbdcce75bd797e167fd1864b7a7
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-08-19 17:36:02 +0700

    update page catalog and accessory

--- thecakery/base/views.py
+++ thecakery/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from cakes.models import Cake, CakeImage, Flavour
 from party_accessories.models import PartyAccessory
+from django.db.models import Q
 
 def home_page(request):
     cakes = Cake.objects.all()
@@ -20,13 +21,23 @@ def contact(request):
     return render(request, 'base/contact_us.html')
 
 def catalog(request):
-    cakes = Cake.objects.prefetch_related('trending_entries__trend_type', 'images').all()
+    query = request.GET.get('q')
+    cakes = Cake.objects.prefetch_related('trending_entries__trend_type', 'images')
+
+    if query:
+        cakes = cakes.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(flavours__name__icontains=query)
+        ).distinct()
+
     user = request.user
 
-    # Pass cakes and user to the template
     context = {
         'cakes': cakes,
         'user': user,
+        'query': query,  # Pass the query to the template to display in the search form
     }
 
     return render(request, 'base/catalog.html', context)
@@ -50,5 +61,10 @@ def dessert(request):
     return render(request, 'base/dessert.html')
 
 def accessory_list(request):
-    accessories = PartyAccessory.objects.all()
-    return render(request, 'base/accessory.html', {'accessories': accessories})
+    query = request.GET.get('q', '')  # Get the search query from the request
+    if query:
+        accessories = PartyAccessory.objects.filter(name__icontains=query)
+    else:
+        accessories = PartyAccessory.objects.all()
+    
+    return render(request, 'base/accessory.html', {'accessories': accessories, 'query': query})
